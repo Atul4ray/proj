07c0f0467468324eff83f13bf7bf14ee074a646c
Atul4ray <atul4ray@gmail.com>
2022-05-14 01:21:12 +0530

    footer, Home, Header Update

---
+++ src/components/layout/Footer.jsx
@@ -0,0 +1,106 @@
+import React from 'react'
+
+const Footer = () => {
+  return (
+    <>
+        <footer>
+  <div className="container">
+    <div className="row">
+      <div className="col-md-3 col-lg-3 col-xl-3">
+        <div className="footer-widget">
+          <div className="footer-logo">
+            <a href="index.html"><img src="assets/img/logo.png" alt="" /></a>
+          </div>
+          <address>
+            <h4>Office</h4>
+            <p>168/170, Avenue 01, Mirpur DOHS, Bangladesh</p>
+          </address>
+          <ul className="social-media-icons">
+            <li><a href="https://www.facebook.com/"><i className="fab fa-facebook-f" /></a></li>
+            <li><a href="https://www.twitter.com/"><i className="fab fa-twitter" /></a></li>
+            <li><a href="https://www.pinterest.com/"><i className="fab fa-pinterest-p" /></a></li>
+            <li><a href="https://www.instagram.com/"><i className="fab fa-instagram" /></a></li>
+          </ul>
+        </div>
+      </div>
+      <div className="col-md-3 col-lg-3 col-xl-3">
+        <div className="footer-widget">
+          <h4>Our Services</h4>
+          <ul className="footer-menu">
+            <li><a href="service-details.html">Strategy &amp; Research</a></li>
+            <li><a href="service-details.html">Web Development</a></li>
+            <li><a href="service-details.html">Web Solution</a></li>
+            <li><a href="service-details.html">Digital Merketing</a></li>
+            <li><a href="service-details.html">App Design</a></li>
+            <li><a href="service-details.html">App Development</a></li>
+          </ul>
+        </div>
+      </div>
+      <div className="col-md-3 col-lg-3 col-xl-3">
+        <div className="footer-widget">
+          <h4>Company</h4>
+          <ul className="footer-menu">
+            <li><a href="about.html">About Us</a></li>
+            <li><a href="services.html">Services</a></li>
+            <li><a href="project.html">Project</a></li>
+            <li><a href="blog.html">Blog</a></li>
+            <li><a href="contact.html">Career</a></li>
+            <li><a href="services.html">Pricing Plan</a></li>
+          </ul>
+        </div>
+      </div>
+      <div className="col-md-3 col-lg-3 col-xl-3">
+        <div className="footer-widget">
+          <h4>Contacts</h4>
+          <div className="number">
+            <div className="num-icon">
+              <i className="fas fa-phone-alt" />
+            </div>
+            <div className="phone">
+              <a href="tel:05661111985">+880 566 1111 985</a>
+              <a href="tel:06571111576">+880 657 1111 576</a>
+            </div>
+          </div>
+          <div className="office-mail">
+            <div className="mail-icon">
+              <i className="far fa-envelope" />
+            </div>
+            <div className="email">
+              <a href="tell:info@example.com"><span className="__cf_email__" data-cfemail="83eaede5ecc3e6fbe2eef3efe6ade0ecee">[email&nbsp;protected]</span></a>
+              <a href="tell:info@support.com"><span className="__cf_email__" data-cfemail="d5bcbbb3ba95a6a0a5a5baa7a1fbb6bab8">[email&nbsp;protected]</span></a>
+            </div>
+          </div>
+          <div className="address">
+            <div className="address-icon">
+              <i className="fas fa-map-marker-alt" />
+            </div>
+            <p>168/170, Avenue 01, Mirpur DOHS, Bangladesh</p>
+          </div>
+        </div>
+      </div>
+    </div>
+    <div className="footer-bottom">
+      <div className="row align-items-center">
+        <div className="col-12 col-md-4 col-lg-4 col-xl-5">
+          <div className="copy-txt">
+            <span>Copyright 2022 <b>Finibus</b> | Design By <a href="#">Egens Lab</a></span>
+          </div>
+        </div>
+        <div className="col-12 col-md-8 col-lg-8 col-xl-7">
+          <ul className="footer-bottom-menu">
+            <li><a href="#">Privacy Policy</a></li>
+            <li><a href="#">Terms of Use</a></li>
+            <li><a href="#">Support Policy</a></li>
+            <li><a href="#">Terms of Service</a></li>
+          </ul>
+        </div>
+      </div>
+    </div>
+  </div>
+</footer>
+
+    </>
+  )
+}
+
+export default Footer
